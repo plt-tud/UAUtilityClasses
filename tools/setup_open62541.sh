3f08fdbcd778608daafbb779ab86d6864b7aef1c
ichrispa <Chris_Paul.Iatrou@tu-dresden.de>
2016-09-01 18:49:40 +0200

    Linking dependant data instead of copying it - editing code now updates the target repo.

--- tools/setup_open62541.sh
+++ tools/setup_open62541.sh
@@ -30,23 +30,14 @@ install)
   fi
   
   log "Installing headers"
-  
-  # Pretend we don't have amalgamation (use bootstrapping code in model): Copy all header files
-  ( cd "$SUBPROJECT_BUILD_DIRERCTORY"/..
-  find ./ -name *.h | while read HEADER; do 
-    DIR=`dirname $HEADER`; 
-    mkdir -p ../../../include/open62541/$DIR; 
-    cp $HEADER ../../../include/open62541/$DIR; 
-  done
-  )
   # Since we are actually using open62541.c, we will still need this one...
-  cp "$SUBPROJECT_BUILD_DIRERCTORY"/open62541.h "$CMAKE_PROJECT_DIRECTORY/include/open62541"
+  ln -s "$SUBPROJECT_BUILD_DIRERCTORY"/open62541.h "$CMAKE_PROJECT_DIRECTORY/include/open62541"
   
-  cp "$SUBPROJECT_BUILD_DIRERCTORY"/open62541.c "$CMAKE_PROJECT_DIRECTORY/src/open62541"
+  ln -s "$SUBPROJECT_BUILD_DIRERCTORY"/open62541.c "$CMAKE_PROJECT_DIRECTORY/src/open62541"
   log "Installing libraries"
-  cp "$SUBPROJECT_BUILD_DIRERCTORY"/libopen62541* "$CMAKE_PROJECT_DIRECTORY/lib"
+  ln -s "$SUBPROJECT_BUILD_DIRERCTORY"/libopen62541* "$CMAKE_PROJECT_DIRECTORY/lib"
 
-  cp -r "$SUBPROJECT_BUILD_DIRERCTORY/../tools/pyUANamespace" "$CMAKE_PROJECT_DIRECTORY/tools"
+  ln -s -r "$SUBPROJECT_BUILD_DIRERCTORY/../tools/pyUANamespace" "$CMAKE_PROJECT_DIRECTORY/tools"
   rm "$CMAKE_PROJECT_DIRECTORY"/tools/pyUANamespace/*txt
   ;;
 uninstall)
