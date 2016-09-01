978b576d0e9cfa49cae08e39f5fd59cab15b0027
ichrispa <Chris_Paul.Iatrou@tu-dresden.de>
2016-09-01 17:11:12 +0200

    Updated ua proxies (includes arrays & nodeid proxies) as well as typeconversions.

--- include/ua_proxies_typeconversion.h
+++ include/ua_proxies_typeconversion.h
@@ -60,6 +60,16 @@ _p_cppstring = s;\
 free(s);\
 } while(0);
 
+/* CPPSTRING_TO_UASTRING(UA_String s_ua, std::string s_cpp)
+ * 
+ * Copy contents of s_cpp into s_ua
+ */
+#define CPPSTRING_TO_UASTRING(_p_uastring, _p_cppstring) {\
+const char *s  = _p_cppstring.c_str(); \
+_p_uastring.length = _p_cppstring.length(); \
+_p_uastring.data = (UA_Byte *) malloc(_p_uastring.length); \
+memcpy(_p_uastring.data, s, _p_uastring.length); \
+}
 
 #define NODE_BROWSENAME_AS_STRING(_p_server, _p_nodeid, _p_strbuffer) { \
 UA_QualifiedName _p_tmpName; \
@@ -79,15 +89,6 @@ UA_Server_readDisplayName(_p_server, _p_nodeid, &_p_tmpName); \
 UASTRING_AS_STRING(_p_tmpName.text,  _p_strbuffer); \
 }
 
-/* CPPSTRING_TO_UASTRING(UA_String s_ua, std::string s_cpp)
- * 
- * Copy contents of s_cpp into s_ua
- */
-#define CPPSTRING_TO_UASTRING(_p_uastring, _p_cppstring) {\
-const char *s  = _p_cppstring.c_str(); \
-_p_uastring.length = _p_cppstring.length(); \
-_p_uastring.data = (UA_Byte *) malloc(_p_uastring.length); \
-memcpy(_p_uastring.data, s, _p_uastring.length); \
-}
+
 
 #endif //#ifndef HAVE_UA_PROXIES_TYPECONVERSION_H
